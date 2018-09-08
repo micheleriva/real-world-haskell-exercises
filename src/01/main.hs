ab7956b1d6505c426523e4ee7ffd9543666e91f6
Michele Riva <master@micheleriva.it>
2018-09-08 23:20:09 +0200

    fixed 01

--- src/01/main.hs
+++ src/01/main.hs
@@ -1,4 +1,5 @@
 -- Page 14
 
 main = interact wordCount
-  where wordCount input = show (length (words input)) ++ "\n"
+  where wordCount input = show (length (word input)) ++ "\n"
+  -- remove "word" function if you want to get characters length.
